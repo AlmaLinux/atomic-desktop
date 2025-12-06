19571814453590b5f3efdae2281cd05166657b7d
Alex Iribarren <alex.iribarren@gmail.com>
2025-12-06 10:01:46 +0100

    Find the package name directly

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -6,6 +6,7 @@ dnf install -y 'dnf-command(config-manager)' epel-release
 dnf config-manager --set-enabled crb
 
 # EPEL ships it's own epel-release package, let's make sure we've got that one
-dnf upgrade -y --best --allowerasing epel-release
+# We've got to do it this way because the package is named differently in x86_64_v2
+dnf upgrade -y $(dnf repoquery --installed --qf '%{name}' --whatprovides epel-release)
 
 dnf install -y system-reinstall-bootc
